e983c574e57b0b2f3f18147fbaaa5b09f0c20bdf
muzudho <muzudho1@gmail.com>
2026-05-31 12:01:41 +0900

    粒度を揃えた

--- ShogiTournamentSystemAnalyzer/Application/Execution/ApplicationRun.cs
+++ ShogiTournamentSystemAnalyzer/Application/Execution/ApplicationRun.cs
@@ -3,21 +3,11 @@
  */
 namespace ShogiTournamentSystemAnalyzer.Application.Execution;
 
-using ShogiTournamentSystemAnalyzer.Application.Input;
-
 internal static class ApplicationRun
 {
     internal static void Run(IReadOnlyList<string> args)
     {
         ApplicationStartup.Start();
-
-        // ［依頼］が［要求ファイル］からか、［対話か］に応じて、入力導線を準備するぜ（＾▽＾）！
-        using var inputSource = InputSourceConfiguration.ConfigureInputSource(args);
-
-        // ［依頼］を受け取って分析を始めるぜ（＾▽＾）！
-        Analysis.Run();
-
-        // 入力セッションの後片付けや完了処理を行うぜ（＾▽＾）！
-        inputSource.Complete();
+        ApplicationRequestRun.Run(args);
     }
 }
